c94a1dd701ac9b36c1afbaf3bffda3d6c32a3f34
23hh <lsh6166@gmail.com>
2025-12-07 22:57:50 +0900

    12/07 정리

---
+++ section3/chapter2.ts
@@ -0,0 +1,10 @@
+// down Cast
+// let num1: number = 10;
+// let num2: 10 = 10;
+
+// num2 = num1; // Error: Type 'number' is not assignable to type '10'
+
+// up Cast
+let num3: 20 = 20;
+let num4: number;
+num4 = num3; // OK
